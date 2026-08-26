9877999314b6ebd56917c047329db2095f4f823c
Tiago <tiagofernandessaracol@gmail.com>
2026-08-26 19:34:25 -0300

    Adiciona leitura de eventos de log
    git add .
    cd
    step
    back
    exit

--- src/main/java/com/socassistant/Main.java
+++ src/main/java/com/socassistant/Main.java
@@ -1,7 +1,18 @@
 package com.socassistant;
-public class Main{
- public static void main(String[] args){
-  System.out.println("SOC Assistant iniciado!");
+ import java.io.IOException;
+ import java.nio.file.Files;
+ import java.nio.file.Path;
 
+ public class Main{
+  public static void main(String[] args){
+   Path caminho = Path.of("logs/eventos.log");
+  try{
+	for(String linha: Files.readAllLines(caminho)){
+		System.out.println(linha);
+}
+}
+catch(IOException erro){
+	System.out.println("Erro ao ler o arquivo de log.");
+ }
  }
 }
